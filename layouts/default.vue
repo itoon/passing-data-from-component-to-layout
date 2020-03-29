907c82b47cccd169f1032e4686d67d8302aa39ae
Songklod Saengvoratip <songklod@marvelous.studio>
2020-03-29 15:49:49 +0700

    index emit

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,9 +1,27 @@
 <template>
-  <div>
-    <nuxt />
+  <div class="pt-5">
+    <div class="container">
+      <div class="row">
+        <div class="col-12">
+          <div class="text-center">
+            <h1>{{ title }}</h1>
+          </div>
+        </div>
+      </div>
+    </div>    
+    <nuxt @changeTitle="title = $event" />    
   </div>
 </template>
 
+<script>
+export default {
+  data(){
+    return {
+      title: 'Title from Layout'
+    }
+  }
+}
+</script>
 <style>
 html {
   font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
---
+++ pages/parent.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-12">
+        <div class="pt-5">
+          <div class="text-center">
+            {{ age }}
+          </div>
+        </div>
+      </div>      
+    </div>    
+  </div>
+</template>
+
+<script>
+export default {
+  data(){
+    return {
+      age: '29'
+    }
+  }
+}
+</script>
